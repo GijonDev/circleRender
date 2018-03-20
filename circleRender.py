bff56c035d7d80bb11e228db8a753ce9cc73e3c1
Juan Mier <underscoreBis@gmail.com>
2018-03-20 21:18:34 +0100

    Add files via upload

--- circleRender.py
+++ circleRender.py
@@ -6,27 +6,42 @@ jony = turtle.Turtle()
 jony.color("red", "black")
 
 
-circleSize = 250
-circleSpeed = 50
-circleLimit = 999
+circleSize = 1
+circleSpeed = 90
+circleLimit = 99999
+circleDeg = 50
+circleSum = 1
 
-#defaultSettings
+###defaultSettings
 #circleSize = 250
-#circleSize = 50
-#circleLimit = 999
+#circleSpeed = 50
+#circleLimit = 99999
+#circleDeg = "randomDeg"
+#circleSum = 0
 
+if circleDeg=="randomDeg":
+    del circleDeg
 
 
 turtle.tracer(circleSpeed)
 for i in range (1,circleLimit):
     randomDeg=random.randint(45, 90)
+    if not 'circleDeg' in locals():
+        circleDeg=randomDeg
+        randomDegEnable=1
+    else:
+        randomDegEnable=0
+
+    if randomDegEnable==1:
+        circleDeg=randomDeg
     
     
     for i in range (1,5):
         jony.circle(circleSize)
         jony.left(90)
         
-    jony.left(randomDeg)
+    jony.left(circleDeg)
+    circleSize=circleSize+circleSum
 
     if jony.color() == ("red", "black"):
         jony.color("orange", "black")
